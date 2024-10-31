0ab561b1d96ad33f991ce285aaa47b190d05c962
Marya <marya.sdk@gmail.com>
2024-10-31 12:32:16 +0100

    feat(blog/home): improve section semantics

--- app/(blog)/page.tsx
+++ app/(blog)/page.tsx
@@ -6,25 +6,28 @@ import PortableText from "./portable-text";
 import type { Settings } from "@/sanity.types";
 import { sanityFetch } from "@/sanity/lib/fetch";
 import { settingsQuery } from "@/sanity/lib/queries";
+import { Main } from "@/components/layout";
 
-function Intro({ settings }: { settings: Settings }) {
+function IntroHeader({ settings }: { settings: Settings }) {
   const { title, subheader, introTitle, introContent } = settings;
   return (
-    <section className="mt-16 mb-16">
-      <div className="flex flex-col items-center lg:mb-12 lg:flex-row lg:justify-between">
-        <h1 className="text-balance text-6xl font-bold leading-tight tracking-tighter lg:pr-8 lg:text-8xl">
-          {title}
-        </h1>
-        <h2 className="text-pretty mt-5 text-center text-lg lg:pl-8 lg:text-left">
-          <PortableText className="prose-lg" value={subheader as any} />
-        </h2>
+    <header className="mt-16 mb-16">
+      <div className="container mx-auto px-5">
+        <div className="flex flex-col items-center lg:mb-12 lg:flex-row lg:justify-between">
+          <h1 className="text-balance text-6xl font-bold leading-tight tracking-tighter lg:pr-8 lg:text-8xl">
+            {title}
+          </h1>
+          <h2 className="text-pretty mt-5 text-center text-lg lg:pl-8 lg:text-left">
+            <PortableText className="prose-lg" value={subheader as any} />
+          </h2>
+        </div>
+        <div className="my-20">
+          <h2 className="text-2xl font-bold">{introTitle}</h2>
+          <br />
+          <p>{introContent}</p>
+        </div>
       </div>
-      <div className="my-20">
-        <h2 className="text-2xl font-bold">{introTitle}</h2>
-        <br />
-        <p>{introContent}</p>
-      </div>
-    </section>
+    </header>
   );
 }
 
@@ -34,16 +37,18 @@ export default async function Page() {
   });
 
   return (
-    <div className="container mx-auto px-5">
-      <Intro settings={settings as Settings} />
-      <aside>
-        <h2 className="mb-8 text-6xl font-bold leading-tight tracking-tighter md:text-7xl">
-          Compilations
-        </h2>
-        <Suspense>
-          <MoreStories skip={""} limit={100} />
-        </Suspense>
-      </aside>
-    </div>
+    <>
+      <IntroHeader settings={settings as Settings} />
+      <Main>
+        <aside>
+          <h2 className="mb-8 text-6xl font-bold leading-tight tracking-tighter md:text-7xl">
+            Compilations
+          </h2>
+          <Suspense>
+            <MoreStories skip={""} limit={100} />
+          </Suspense>
+        </aside>
+      </Main>
+    </>
   );
 }
