71f257eb08653bde14bbd39e1693c14836935a73
Marya <marya.sdk@gmail.com>
2024-10-31 12:41:44 +0100

    feat(Playground): create website section

--- components/layout/Main.tsx
+++ components/layout/Main.tsx
@@ -1,11 +1,17 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
 interface MainProps {
+  contentClassName?: React.HTMLAttributes<HTMLElement>["className"];
   children: React.ReactNode;
 }
 
-export default function Main({ children }: MainProps) {
+export default function Main({ contentClassName, children }: MainProps) {
   return (
     <main className="flex-grow flex-auto bg-background">
-      <div className="container mx-auto px-5 py-8">{children}</div>
+      <div className={twMerge("container mx-auto px-5 py-8", contentClassName)}>
+        {children}
+      </div>
     </main>
   );
 }
